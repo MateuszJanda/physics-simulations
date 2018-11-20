cb960a783630c055b736aee57819543277062bb0
Mateusz Janda <mateusz.janda@gmail.com>
2018-11-20 21:26:47 +0100

    collision.py refactor

--- collision.py
+++ collision.py
@@ -1,48 +1,38 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from vpython import *
+import vpython as vp
 import math
-#from vpython.graph import *
 
 
 TOLERANCE = 0.2
 LINEAR_DRAG_COEFFICIENT = 0.2
 DENSITY_OF_AIR = 1.168  # kg/m^3
-OVER_THE_BODY = vector(0, 0, 1)
+POS_OVER_THE_BODY = vp.vector(0, 0, 1)
+
 
 def main():
-    setupDisplay()
+    setup_display()
 
-    body1 = cylinder(pos=vector(-5, 0, 0), axis=vector(0, 0, 1), radius=1)
-    body1.mass = 10  # kg
-    body1.projectedArea = 10  # m^2
-    body1.vel = vector(0, 0, 0)
-
-    body2 = cylinder(pos=vector(3, 0.5, 0), axis=vector(0, 0, 1), radius=1)
-    body2.mass = 10  # kg
-    body2.projectedArea = 10  # m^2
-    body2.vel = vector(0, 0, 0)
-
-
-    arrRed, arrBlue = getArrows()
+    body1, body2 = create_bodies()
+    red_arrow, blue_arrow = create_arrows()
 
     t = 0
     freq = 100
-    dt = 1.0 / freq
+    dt = 1/freq
 
     while t < 12:
-        rate(freq)
+        vp.rate(freq)
 
         if t > 0 and t < 1:
-            F1 = 100 * vector(1, 0, 0)
+            F1 = 100 * vp.vector(1, 0, 0)
         else:
-            F1 = vector(0, 0, 0)
+            F1 = vp.vector(0, 0, 0)
 
-        F2 = vector(0, 0, 0)
+        F2 = vp.vector(0, 0, 0)
 
-        updateBody(body1, F1, arrRed, dt)
-        updateBody(body2, F2, arrBlue, dt)
+        updateBody(body1, F1, red_arrow, dt)
+        updateBody(body2, F2, blue_arrow, dt)
 
         if checCollision(body1, body2):
             collision(body1, body2)
@@ -50,39 +40,54 @@ def main():
         t += dt
 
 
-def setupDisplay():
-    canvas(x=0, y=0, width=400, height=400,
+def setup_display():
+    vp.canvas(x=0, y=0, width=400, height=400, center=vp.vector(0, 0, 0),
         userzoom=False, userspin=True, autoscale=False,
-        center=vector(0, 0, 0), foreground=color.white, background=color.black)
+        foreground=vp.color.white, background=vp.color.black)
 
 
-def getArrows():
-    arrRed = arrow(pos=vector(0, 0, 0), shaftwidth=0.5, color=color.red)
-    arrRed.visible = False
-    arrBlue = arrow(pos=vector(0, 0, 0), shaftwidth=0.5, color=color.blue)
-    arrBlue.visible = False
+def create_bodies():
+    body1 = vp.cylinder(pos=vp.vector(-5, 0, 0), axis=vp.vector(0, 0, 1), radius=1)
+    body1.mass = 10  # kg
+    body1.area = 10  # m^2
+    body1.vel = vp.vector(0, 0, 0)  # m/s^2
 
-    return arrRed, arrBlue
+    body2 = vp.cylinder(pos=vp.vector(3, 0.5, 0), axis=vp.vector(0, 0, 1), radius=1)
+    body2.mass = 10  # kg
+    body2.area = 10  # m^2
+    body2.vel = vp.vector(0, 0, 0)  # m/s^2
+
+    return body1, body2
 
 
-def updateBody(body, Ftrust, arr, dt):
-    F = vector(0, 0, 0)
+def create_arrows():
+    red_arrow = vp.arrow(pos=vp.vector(0, 0, 0), shaftwidth=0.5, color=vp.color.red)
+    red_arrow.visible = False
+
+    blue_arrow = vp.arrow(pos=vp.vector(0, 0, 0), shaftwidth=0.5, color=vp.color.blue)
+    blue_arrow.visible = False
+
+    return red_arrow, blue_arrow
+
+
+def updateBody(body, thrust, arrow, dt):
+    force = vp.vector(0, 0, 0)
 
     # Poniżej pewnego progu nie obliczamy prędkości stycznej
     if body.vel.mag > TOLERANCE:
-        R = -body.vel.norm() * LINEAR_DRAG_COEFFICIENT * 0.5 * DENSITY_OF_AIR * body.vel.mag**2 * body.projectedArea
-        F += R
+        R = -body.vel.norm() * LINEAR_DRAG_COEFFICIENT * 0.5 * DENSITY_OF_AIR * body.vel.mag**2 * body.area
+        force += R
 
-    if Ftrust.mag > 0:
-        arr.pos = OVER_THE_BODY + body.pos
-        arr.axis = 4 * Ftrust.norm()
-        arr.visible = True
+    if thrust.mag > 0:
+        arrow.pos = POS_OVER_THE_BODY + body.pos
+        arrow.axis = 4 * thrust.norm()
+        arrow.visible = True
     else:
-        arr.visible = False
+        arrow.visible = False
 
-    F += Ftrust
+    force += thrust
 
-    body.acc = F/body.mass
+    body.acc = force/body.mass
     body.vel += body.acc * dt
     body.pos += body.vel * dt
 
@@ -97,7 +102,7 @@ def checCollision(body1, body2):
     collisionNorm = d.norm()
     relativVel = body1.vel - body2.vel
 
-    vrn = dot(relativVel, collisionNorm)
+    vrn = vp.dot(relativVel, collisionNorm)
     if math.fabs(s) <= CTOL and vrn < 0.0:
         # zderzenie
         return 1
@@ -106,6 +111,7 @@ def checCollision(body1, body2):
         return -1
     # bez zderzenia
     return 0
+
 
 def collision(body1, body2):
     e = 0.5
@@ -117,7 +123,7 @@ def collision(body1, body2):
     # j = ( -(1+e) * (dot(relativVel, collisionNorm))) / ( dot(collisionNorm, collisionNorm) * (1/body1.mass + 1/body2.mass))
     # Ale dot(collisionNorm, collisionNorm) i tak zawsze da wynik 1.0
     # Moja wersja
-    j = ( -(1+e) * (dot(relativVel, collisionNorm))) / ( (1/body1.mass + 1/body2.mass))
+    j = ( -(1+e) * (vp.dot(relativVel, collisionNorm))) / ( (1/body1.mass + 1/body2.mass))
     body1.vel += j * collisionNorm / body1.mass
     body2.vel -= j * collisionNorm / body2.mass
 

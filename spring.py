ada69d6f2ad511b1542f59d30d197a9ed8f46b72
Mateusz Janda <mateusz.janda@gmail.com>
2018-11-25 22:58:03 +0100

    Cleanup

--- spring.py
+++ spring.py
@@ -10,96 +10,82 @@ http://www.iwiedza.net/wiedza/114.html
 https://github.com/gcschmit/vpython-physics/blob/master/energy%20of%20mass%20on%20spring/massOnSpring.py
 """
 
-from vpython import *
+import vpython as vp
 import math
 
-import os
-if os.name == 'nt':
-    import ImageGrab
-
-
-class PrintScreen:
-    def __init__(self):
-        self.frameNum = 0
-
-    def grabImage(self):
-        if os.name != 'nt':
-            return
-
-        fileName = 'img-' + '{fr:04d}'.format(fr=self.frameNum) + '.png'
-        im = ImageGrab.grab((0, 0, 400, 400))
-        im.save(fileName)
-
-        self.frameNum += 1
 
 GRAVITY = 9.81  # m/s^2
 
 
 def main():
-    plt1, plt2 = setupDisplay()
-    screen = PrintScreen()
-
-    anchorY = 4
-    # Sufit, do którego podwieszamy sprężynę
-    box(pos=vector(0, anchorY + 0.5, 0), length=6, height=1, width=6)
-
-    springLengthInitial = 3
-    # Na początku spręzyna nie jest ani naciągnięta ani ściśnięta
-    spring = helix(pos=vector(0, anchorY, 0), axis=vector(0, -springLengthInitial + 1, 0),
-                   thickness=1/10, radius=1)
-    # Jedna wartość dla stałej sprężystości i tłumienia
-    spring.constant = 5  # N/m
-
-    h = 2
-    sInit = vector(0, anchorY - spring.length - h/2, 0)
-    mbox = box(pos=sInit, length=2, height=h, width=2)
-    mbox.mass = 2  # kg
-    mbox.vel = vector(0, 0, 0)
+    plt1, plt2 = setup_display()
+    spring, load = create_bodies()
 
     freq = 100
-    dt = 1.0 / freq
+    dt = 1/freq
     t = 0
 
     while t < 3000:
-        rate(freq)
+        vp.rate(freq)
 
-        Fg = GRAVITY * mbox.mass * vector(0, -1, 0)
+        Fg = GRAVITY * load.mass * vp.vector(0, -1, 0)
 
-        springDisplacement = spring.length - springLengthInitial
-        Fk = -spring.constant * springDisplacement * vector(0, -1, 0)
+        springDisplacement = spring.length - spring.init_length
+        Fk = -spring.const * springDisplacement * vp.vector(0, -1, 0)
         F = Fg + Fk
-        mbox.acc = F / mbox.mass
-        mbox.vel += mbox.acc * dt
-        # mbox.pos += mbox.vel * dt + 0.5 * mbox.acc * dt**2
-        mbox.pos += mbox.vel * dt # TODO: który wzór ma mieć tutaj zastosowanie
+        load.acc = F / load.mass
+        load.vel += load.acc * dt
+        # load.pos += load.vel * dt + 0.5 * load.acc * dt**2
+        load.pos += load.vel * dt # TODO: który wzór ma mieć tutaj zastosowanie
 
         plt1.plot(pos=(t, Fk.y))
         plt2.plot(pos=(t, F.y))
 
         # Nowa długość sprężyny
-        # spring.y == anchorY
-        spring.length = spring.pos.y - mbox.pos.y - mbox.height/2
+        # spring.y == celling_y
+        spring.length = spring.pos.y - load.pos.y - load.height/2
 
         # Warunek końca do zapętlenie gif-a
-        if t > 1 and mag(mbox.pos - sInit) < 0.1 and mbox.vel.y < 0:
+        if t > 1 and vp.mag(load.pos - load.init_pos) < 0.1 and load.vel.y < 0:
             pass
 
         t += dt
-        screen.grabImage()
-
-    exit()
 
 
-def setupDisplay():
-    canvas(x=0, y=0, width=400, height=400,
+def setup_display():
+    vp.canvas(x=0, y=0, width=400, height=400,
         userzoom=False, userspin=True, autoscale=False,
-        center=vector(0, 0, 0), foreground=color.white, background=color.black)
+        center=vp.vector(0, 0, 4), foreground=vp.color.white, background=vp.color.black)
 
-    plt1 = gcurve(color=color.green, size=2)
-    plt2 = gcurve(color=color.red, size=2)
+    plt1 = vp.gcurve(color=vp.color.green, size=2)
+    plt2 = vp.gcurve(color=vp.color.red, size=2)
 
     return plt1, plt2
 
+
+def create_bodies():
+    # Celling
+    celling_y = 4
+    vp.box(pos=vp.vector(0, celling_y + 0.5, 0), length=6, height=1, width=6)
+
+    INIT_LENGTH = 3
+    spring = vp.helix(pos=vp.vector(0, celling_y, 0), axis=vp.vector(0, -INIT_LENGTH + 1, 0),
+        thickness=1/10,
+        radius=1,
+        init_length = INIT_LENGTH,
+        const=5)  # N/m - one value for elacstic and dumping factor
+
+    height = 2
+    INIT_POS = vp.vector(0, celling_y - spring.length - height/2, 0)
+    load = vp.box(pos=INIT_POS,
+        init_pos=INIT_POS,
+        height=height,
+        width=2,
+        length=2,
+        mass = 2,  # kg
+        vel = vp.vector(0, 0, 0))
+
+    return spring, load
 
 if __name__ == '__main__':
     main()

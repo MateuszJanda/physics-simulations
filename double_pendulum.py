77e25dfff2172c31f2211e97da98631b76672a65
Mateusz Janda <mateusz.janda@gmail.com>
2019-08-16 23:48:43 +0200

    Hand notes

--- double_pendulum.py
+++ double_pendulum.py
@@ -28,7 +28,7 @@ def main():
     while True:
         vp.rate(100)
 
-        step_simulation(dt, rod1, rod2)
+        step_simulation_notes(dt, rod1, rod2)
 
         # povexport.export(scene, filename='img-%04d.pov' % frame,
         #   include_list=['colors.inc', 'stones.inc', 'woods.inc', 'metals.inc'])
@@ -140,6 +140,41 @@ def step_simulation_sympy(dt, rod1, rod2):
     rod2.axis = rod2.length * vp.vector(math.sin(rod2.theta), -math.cos(rod2.theta), 0)
 
 
+def step_simulation_notes(dt, rod1, rod2):
+    """
+    Hand notes.
+    Credits:
+    https://www.myphysicslab.com/pendulum/double-pendulum-en.html
+    """
+    from math import sin, cos
+
+    g = GRAVITY_ACC
+    m1, m2 = rod1.mass, rod2.mass
+    L1, L2 = rod1.length, rod2.length
+    t1, t2 = rod1.theta, rod2.theta
+    t1_d1, t2_d1 = rod1.d1_theta, rod2.d1_theta
+
+    t1_d2 = -(L1*m2*t1_d1**2*sin(2*t1 - 2*t2)/2 + L2*m2*t2_d1**2*sin(t1 - t2) + g*m1*sin(t1) + g*m2*sin(t1)/2 + g*m2*sin(t1 - 2*t2)/2)/(L1*(m1 - m2*cos(t1 - t2)**2 + m2))
+    t1_d1 += t1_d2 * dt
+    t1 += t1_d1 * dt
+
+    t2_d2 = ((-sin(t2)*t1_d1**2*L1*sin(t1) - sin(t2)*t2_d1**2*L2*cos(t2) - sin(t2)*g + cos(t2)*t1_d1**2*L1*sin(t1) + cos(t2)*t2_d1**2*L2*sin(t2))*(m1 + m2) + g*(m2*sin(t1) + m1*sin(t1))*(cos(t2)*cos(t1) + sin(t2)*sin(t1))) / ((m1 + m2) - m2*(cos(t2)*cos(t1) + sin(t2)*sin(t1))**2)
+    t2_d1 += t2_d2 * dt
+    t2 += t2_d1 * dt
+
+    rod1.d2_theta = t1_d2
+    rod1.d1_theta = t1_d1
+    rod1.theta = t1
+
+    rod2.d2_theta = t2_d2
+    rod2.d1_theta = t2_d1
+    rod2.theta = t2
+
+    rod1.axis = rod1.length * vp.vector(math.sin(rod1.theta), -math.cos(rod1.theta), 0)
+    rod2.pos = rod1.pos + rod1.axis
+    rod2.axis = rod2.length * vp.vector(math.sin(rod2.theta), -math.cos(rod2.theta), 0)
+
+
 def resolve_formula():
     """
     Resolve formula using SymPy. Unfortunately verification doesn't work
@@ -159,7 +194,7 @@ def resolve_formula():
     eq13 = Eq(sin(t1)*(m1*y1_d2 + m2*y2_d2 + m2*g + m1*g) + cos(t1)*(m1*x1_d2 + m2*x2_d2), 0)
     eq16 = Eq(sin(t2)*(m2*y2_d2 + m2*g) + cos(t2)*(m2*x2_d2), 0)
 
-    result = solve([eq13, eq16], [t1_d2, t2_d2], dict=True)
+    result = solve([eq13, eq16], [t1_d1, t2_d2], dict=True)
     print(result)
 
     # [{t1_d2: -(L1*m2*t1_d1**2*sin(2*t1 - 2*t2)/2 + L2*m2*t2_d1**2*sin(t1 - t2) + g*m1*sin(t1) + g*m2*sin(t1)/2 + g*m2*sin(t1 - 2*t2)/2)/(L1*(m1 - m2*cos(t1 - t2)**2 + m2)),
